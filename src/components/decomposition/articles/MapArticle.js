f37d44b7c037fa6a05798b05682ee167011b26f1
Cazuist <sovrus@mail.ru>
2020-12-22 16:06:04 +0300

    Initial commit

---
+++ src/components/decomposition/articles/MapArticle.js
@@ -0,0 +1,8 @@
+export default function MapArticle({ article }) {
+
+  return (
+   <div className="article-content map">
+      {article.content}
+    </div>
+  );
+}
---
+++ src/components/decomposition/banner/Banner.js
@@ -0,0 +1,10 @@
+// Показывает баннер
+
+export default function Banner() {
+  
+  return (
+    <div className="banner">
+      Banner
+    </div>
+  );
+}
